251b4f12f742ec7773068f1f9c37744beffa16d9
agr3fox <118577762+agr3fox@users.noreply.github.com>
2022-12-01 12:20:17 -0800

    Update multiloadstring.lua

--- multiloadstring.lua
+++ multiloadstring.lua
@@ -1,10 +1,11 @@
-# PurpleFox
+# anti-ide
+wait(0)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/agr3fox/PurpleFox/main/anti-ide.lua"))()
 
+# name
 wait(0)
 loadstring(game:HttpGet(""))()
 
-wait(0)
-loadstring(game:HttpGet(""))()
-
+# name
 wait(0)
 loadstring(game:HttpGet(""))()
